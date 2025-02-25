e4fe3d8bb4dc4196d117aa4aeca440f5b314eef8
andrensenn <up202108856@up.pt>
2025-02-25 12:30:06 +0000

    o que tava feito na aula

--- lab2/timer.c
+++ lab2/timer.c
@@ -40,6 +40,35 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
+                          
+  switch(field){
+    case tsf_all:
+      printf("%0x\n",st); 
+      /* Display status byte, in hexadecimal */
+      break;
+    case tsf_initial:
+      int x = (st & (TIMER_LSB | TIMER_MSB))>>4;
+      if(x==1){
+        printf("LSB\n");
+      }
+      else if(x==2){
+        printf("MSB\n");
+      }
+      else if(x==3){
+        printf("LSB followed by MSB\n");
+      }
+      /* Display the initialization mode, only */
+      break;
+    case tsf_mode:
+    /* Display the counting mode, only */
+      break;
+    case tsf_base:
+    /* Display the counting base, only */
+      break;
+    default: 
+      return 1;
+  }
+  
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
 
